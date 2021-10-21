cbba16807e5ba6a04c92f5311be4f953a648fbfd
vojtch <vojta159.studeny@gmail.com>
2021-10-21 20:33:43 +0200

    Best smart contract in the world

--- src/Saygmtome.sol
+++ src/Saygmtome.sol
@@ -2,4 +2,31 @@
 pragma solidity ^0.8.6;
 
 contract Saygmtome {
+
+    address gm_address;
+    uint last_gm;
+
+    modifier OnlyGmAddress() {
+        require(msg.sender == gm_address);
+        _;
+    }
+
+    constructor() {
+        gm_address = msg.sender;
+        last_gm = block.timestamp;    
+    }
+
+    function gm() public payable OnlyGmAddress returns (string memory){
+        if(last_gm + 1 days >= block.timestamp) {
+            last_gm = block.timestamp;
+            return("Gm to you too :)");
+        } else {
+            if(msg.value > 100 gwei) {
+                last_gm = block.timestamp;
+                return("Why didn't you said gm last morning? I'am very sad. At least you sent some ultrasound money...");
+            } else {
+                return("Don't even talk to me.");
+            }
+        }
+    }
 }
--- src/Saygmtome.t.sol
+++ src/Saygmtome.t.sol
@@ -2,21 +2,34 @@
 pragma solidity ^0.8.6;
 
 import "ds-test/test.sol";
-
 import "./Saygmtome.sol";
+
+interface Ihevm {
+    function warp(uint x) external;
+}
 
 contract SaygmtomeTest is DSTest {
     Saygmtome me;
+    address hevm = 0x7109709ECfa91a80626fF3989D68f67F5b1DD12D;
 
     function setUp() public {
         me = new Saygmtome();
     }
 
-    function testFail_basic_sanity() public {
-        assertTrue(false);
+    function test_say_gm() public {
+        string memory returned_value = me.gm();
+        assertEq("Gm to you too :)", returned_value);
     }
 
-    function test_basic_sanity() public {
-        assertTrue(true);
+    function test_say_gm_late_and_pay() public {
+        Ihevm(hevm).warp(block.timestamp + 2 days);
+        string memory returned_value = me.gm{value: 101 gwei}();
+        assertEq("Why didn't you said gm last morning? I'am very sad. At least you sent some ultrasound money...", returned_value);
+    }
+
+    function test_say_gm_late() public {
+        Ihevm(hevm).warp(block.timestamp + 2 days);
+        string memory returned_value = me.gm();
+        assertEq("Don't even talk to me.", returned_value);
     }
 }
